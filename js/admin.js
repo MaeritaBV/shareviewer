c5818ae7410532941abce95636c32984e5250ec2
erwin <erwin@erwins-mbp-2084.fritz.box>
2018-05-08 00:27:38 +0200

    Test js post

--- js/admin.js
+++ js/admin.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
   function saveSettings() {
-    //OC.msg.startSaving('#shareviewer_notifications_msg');
+    OC.msg.startSaving('#shareviewer_notifications_msg');
     var post = $('#frmShareViewerAdmin').serialize();
 
-    //$.post(OC.generateUrl('/apps/shareviewer/settings/adminsettings'), post, function(response) {
-    //  OC.msg.finishedSuccess('#shareviewer_notifications_msg', response.data.message);
-    //});
-    alert('test');
+    $.post(OC.generateUrl('/apps/shareviewer/settings/adminsettings'), post, function(response) {
+      OC.msg.finishedSuccess('#shareviewer_notifications_msg', response.data.message);
+    });
   }
 
   var $frmShareViewerAdmin = $('#frmShareViewerAdmin');
