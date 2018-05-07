1336521da37f26b28d5c88e1570526ea9999e427
erwin <erwin@erwins-mbp-2084.fritz.box>
2018-05-08 01:36:02 +0200

    Test

--- js/admin.js
+++ js/admin.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   function saveSettings() {
     OC.msg.startSaving('#shareviewer_notifications_msg');
     var post = $('#frmShareViewerAdmin').serialize();
-
+alert(post);
     $.post(OC.generateUrl('/apps/shareviewer/settingscontroller/admin'), post, function(response) {
       OC.msg.finishedSuccess('#shareviewer_notifications_msg', response.data.message);
     });
