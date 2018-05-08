4ff22b7303b70c17961b76087e0e68201269020e
erwin <erwin@192.168.1.90>
2018-05-08 13:51:19 +0200

    Test user id

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -138,6 +138,7 @@ class PageController extends Controller {
     } // while ($shareListRow = $shareListStatement->fetch()) {
 
     $params = ['viewtype'  => $viewtype,
+               'user'      => $this->user,
                'resultset' => serialize($shareListResultSet)];
 
     return new TemplateResponse('shareviewer', 'index', $params);  // templates/index.php
